31bf378adb0d79e4af8953f591fcd4e63ce686d3
Sumani Alima <sumanialima2@gmail.com>
2025-02-10 20:03:30 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("Hello world")
+let pin = "1234";
+
+let atmBalance = 1000;
+let userBalance = 3000;
+let userName ="Habiba"
+
+console.log("Atm Withdrawal Receipt")
+console.log("-----------")
+console.log("User Name is "+ userName)
+console.log("Use Pin is"+ pin)
+console.log("Previous Account Balance"+userBalance)
+console.log("Withdrawal is"+ withdrawalAmount)
+console.log("atmBalance-withdrawalAmount")
+console.log(" userBalance-withdrawalAmount")
+
+    
+let newUserBalance = userBalance-withdrawalAmount;   
+console.log("New Account Balance: GHC" + newUserBalance)
---
+++ arithmetic.js
@@ -0,0 +1,11 @@
+let signal="green"
+if(signal==="red") {
+    console.log("stop")
+
+}else if (signal==="yellow"){
+    console.log("Get ready")
+}else if(signal==="green"){
+    console.log("Go")
+}else{
+    console.log("Nothing")
+}
